bfd267c0608326b35f0c213aac01640441b3cf06
ElectroManBY <oleg.starynski@gmail.com>
2024-01-13 19:21:58 +0300

    Add class Solution and metod

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+
+       return "";
+
+    }
+};
+
+Solution solution;
 
 int main()
 {
 
+    vector<string> strs{"flower","flow","flight"};
+    string answ = solution.longestCommonPrefix(strs);
+
+    std::cout << answ << std::endl;
     return 0;
 }
